451bdbd64f817b6325b9d0cf90a0d6181145fff0
Darren Beattie <darren@fettle.ca>
2020-12-09 11:10:35 -0800

    fix date parsing syntax error

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -44,7 +44,7 @@ router.route('/update/:id').post((req, res) => {
       exercise.username = req.body.username;  
       exercise.description = req.body.description;  
       exercise.duration = Number(req.body.duration);  
-      exercise.date = _Date_.parse(req.body.date); exercise.save()  
+      exercise.date = Date.parse(req.body.date); exercise.save()  
         .then(() => res.json('Exercise updated!'))  
         .catch(err => res.status(400).json('Error: ' + err));  
     })  
