1fd14f0b359877cab6182b6b86f7058fbc737526
Darren Beattie <darren@fettle.ca>
2021-04-16 11:33:56 -0700

    added chakra ui & created a navbar

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,31 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
+import { Box, Heading, Flex, Text, Button } from "@chakra-ui/react";
 
-export default function Navbar() {
+const MenuItems = ({ children }) => (
+  <Text mt={{ base: 4, md: 0 }} mr={6} display="block">
+    {children}
+  </Text>
+);
+
+export default function Navbar(props) {
+  const [show, setShow] = useState(false);
+  const handleToggle = () => setShow(!show);
+  
   return (
-    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-      <Link to="/" className="navbar-brand">
-        ExcerTracker
-      </Link>
-      <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-            <Link to="/" className="nav-link">
-              Exercises
-            </Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/create" className="nav-link">
-              Create Exercise Log
-            </Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/user" className="nav-link">
-              Create User
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      bg="teal.500"
+      color="white"
+      {...props}
+    >
+      <Flex align="center" mr={5}>
+        <Link to="/" >
+          <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
+            ExcerTracker
+          </Heading>
+        </Link>
+      </Flex>
+
+      <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
+        <svg
+          fill="white"
+          width="12px"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </Box>
+
+      <Box
+        display={{ sm: show ? "block" : "none", md: "flex" }}
+        width={{ sm: "full", md: "auto" }}
+        alignItems="center"
+        flexGrow={1}
+      >
+        <MenuItems><Link to="/">Exercises</Link></MenuItems>
+        <MenuItems><Link to="/create">Create Exercise Log</Link></MenuItems>
+        <MenuItems><Link to="/user">Create User</Link></MenuItems>
+      </Box>
+
+      <Box
+        display={{ sm: show ? "block" : "none", md: "block" }}
+        mt={{ base: 4, md: 0 }}
+      >
+        <Button bg="transparent" border="1px">
+          Create account
+        </Button>
+      </Box>
+    </Flex>
   );
-}
+};
